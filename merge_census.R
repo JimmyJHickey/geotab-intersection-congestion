fe7b3cb13a3d27316e470eaebdc2e09f0d2d0799
AlvinSheng <as133@rice.edu>
2019-10-04 12:57:51 -0400

    my_append_geoid is a success, as well as the merging of population data.
    Now I will apply my_append_geoid to the other cities

--- merge_census.R
+++ merge_census.R
@@ -84,10 +84,6 @@ IL_boundary_file <- "tl_2019_17_bg/tl_2019_17_bg.shp"
 
 
 
-
-
-
-
 # append the geoid to train_Chicago
 
 tic()
@@ -104,68 +100,7 @@ toc()
 
 
 
-
-
-
-# # get the GEOID
-# 
-# geoid <- as.numeric(as.character(IL_boundary$GEOID))
-# 
-# # Convert shapefile into Polygons object
-# 
-# IL_coord <- as_Spatial(IL_boundary$geometry)
-# 
-# 
-# 
-# # Extract a mapping between OBJECTID (block group number) and its longitude/latitude polygon
-# 
-# bg <- lapply(
-#   IL_coord@polygons,
-#   function(x) {
-#     pre_coords <- x@Polygons[[1]]@coords
-#     coords <- data.frame(latitude = pre_coords[,2], longitude = pre_coords[,1])
-#     pre_id <- x@ID
-#     id <- as.integer((gsub("ID", "", pre_id))) # this is simply to get the order of block group (1st, 100th, etc.)
-#     list(id = geoid[id], coords = coords)
-#   }
-# )
-# 
-# 
-# 
-# # This will assign each Chicago intersection a geoid
-# 
-# geoid <- vector("integer")
-# 
-# 
-# 
-# # Takes about 17 minutes
-# 
-# tic()
-# 
-# for (i in 1:length(bg)){
-#   x = bg[[i]]$coords 
-#   indicator = point.in.polygon(train_Chicago$Latitude, train_Chicago$Longitude, x$latitude, x$longitude) == 1
-#   geoid[indicator] <- bg[[i]]$id
-# }
-# 
-# toc()
-# 
-# 
-# 
-# # attach geoid to train_Chicago
-# 
-# train_Chicago$geoid <- geoid
-
-
-
-
-
-
-
-
-
-
-# read in population data
+#### merging in population data ####
 
 chicago_popl <- read.csv("/Users/Alvin/Documents/NCSU_Fall_2019/geotab-intersection-congestion/ACS_17_5YR_B01003_with_ann.csv", 
                          header = TRUE, skip = 1)
@@ -176,7 +111,12 @@ names(chicago_popl)[names(chicago_popl) == "Id2"] <- "geoid"
 
 train_Chicago <- merge(train_Chicago, chicago_popl[c("geoid", "Estimate..Total")], by = "geoid", all.x = TRUE)
 
-save(train_Chicago, file = "backup_data_files/train_Chicago.RData")
+# save(train_Chicago, file = "backup_data_files/train_Chicago.RData")
+
+
+
+
+
 
 
 
