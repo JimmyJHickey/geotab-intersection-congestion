c7c8d14f3d73f3fd52f6cc21e4ada9396c5d9c19
AlvinSheng <as133@rice.edu>
2019-10-18 17:24:08 -0400

    Getting weather data for each city

--- merge_weather.R
+++ merge_weather.R
@@ -1,17 +1,64 @@
 
-# devtools::install_github("ropensci/rnoaa")
+library(riem)
+library(drake)
+library(dplyr)
 
-library(rnoaa)
+# load dependencies
+loadd(train_Atlanta)
+loadd(train_Boston)
+loadd(train_Chicago)
+loadd(train_Philadelphia)
 
-chicago_station <- "GHCND:US1ILCK0145"
+loadd(train_normalize_census)
+loadd(test_normalize_census)
 
-NOAA_KEY <- "hYDbNtXOsizrXxbEcBmXdMQtRHRAbJQm"
+# returns the station id of the weather station closest to the intersections in the city
 
-ncdc(datasetid = 'GHCND', stationid = 'GHCND:USW00014895', startdate = '2013-10-01',
-     enddate = '2013-12-01')
+closest_weather_station <- function(city_dat, network) {
+  
+  med_lat <- median(city_dat$Latitude)
+  med_lon <- median(city_dat$Longitude)
+  
+  city_stations <- riem_stations(network = network)
+  
+  distances <- sqrt((city_stations$lat - med_lat)^2 + (city_stations$lon - med_lon)^2)
+  
+  station_id <- city_stations$id[which.min(distances)]
+  
+}
 
 
 
-test_res <- ncdc(datasetid = 'GHCNDMS', stationid = 'GHCND:US1ILCK0012', startdate = '2016-02-01',
-     enddate = '2016-03-01', limit = 1000)
+# res <- riem_measures("ORD", date_start = "2018-06-01")
+
+merge_weather_plan <- drake_plan(
+
+  station_id_Atlanta = closest_weather_station(train_Atlanta, "GA_ASOS"),
+  station_id_Boston = closest_weather_station(train_Boston, "MA_ASOS"),
+  station_id_Chicago = closest_weather_station(train_Chicago, "IL_ASOS"),
+  station_id_Philadelphia = closest_weather_station(train_Philadelphia, "PA_ASOS"),
+  
+  # getting weather data
+  weather_Atlanta = riem_measures(station_id_Atlanta, date_start = "2018-05-01", date_end = "2019-01-31"),
+  weather_Boston = riem_measures(station_id_Boston, date_start = "2018-05-01", date_end = "2019-01-31"),
+  weather_Chicago = riem_measures(station_id_Chicago, date_start = "2018-05-01", date_end = "2019-01-31"),
+  weather_Philadelphia = riem_measures(station_id_Philadelphia, date_start = "2018-05-01", date_end = "2019-01-31")
+  
+)
+
+
+
+config <- drake_config(merge_weather_plan)
+
+vis_drake_graph(config)
+
+
+
+make(merge_weather_plan)
+
+history <- drake_history(analyze = TRUE)
+
+cache <- drake_cache()
+
+
 
