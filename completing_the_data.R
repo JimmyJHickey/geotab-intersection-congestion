76b1d94524afe6f550762d507ae0981914ae197e
AlvinSheng <as133@rice.edu>
2019-11-01 09:35:09 -0400

    Incorporating Jimmy's turn angles

--- completing_the_data.R
+++ completing_the_data.R
@@ -1,11 +1,8 @@
+# this script file will run thru the entire pipeline, doing all the merging of extra data and features to the original train and test
 
 # later, put the whole drake plan in here
 
 source("merge_distCBD.R")
-
-
-
-# this script file will run thru the entire pipeline, doing all the merging of extra data and features to the original train and test
 
 load("backup_data_files/train_append_weather.RData")
 
@@ -23,7 +20,13 @@ test_merge_DistCBD = res$test_merge_DistCBD
 
 
 
-
-
 # merge in Jimmy's feature engineering
+
+train_turn_angle = read.csv("feature_engineering/train_turn-angle.csv", header = FALSE, col.names = c("RowId", "TurnAngle"))
+test_turn_angle = read.csv("feature_engineering/test_turn-angle.csv", header = FALSE, col.names = c("RowId", "TurnAngle"))
+
+train_merge_TurnAngle = merge(train_merge_DistCBD, train_turn_angle, by = "RowId")
+test_merge_TurnAngle = merge(test_merge_DistCBD, test_turn_angle, by = "RowId")
+
+
 
