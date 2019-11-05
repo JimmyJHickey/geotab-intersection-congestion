84abab2aa2e21d70a93fe89fac6ac0ba9516cdb1
AlvinSheng <as133@rice.edu>
2019-11-04 22:00:00 -0500

    incorporated completing_the_data into the drake workflow.
    Also uploading imps.RData before I rerun the random forest

--- completing_the_data.R
+++ completing_the_data.R
@@ -1,8 +1,12 @@
-# this script file will run thru the entire pipeline, doing all the merging of extra data and features to the original train and test
 
-# later, put the whole drake plan in here
+library(dplyr)
+library(drake)
 
-source("merge_distCBD.R")
+# this script file will run thru a pipeline, doing the merging of extra data and features to the original train and test
+
+# load dependencies
+
+source("merge_distCBD.R") # sourcing in Zhen's merge_distCBD function
 
 load("backup_data_files/train_append_weather.RData")
 
@@ -10,52 +14,83 @@ load("backup_data_files/test_append_weather.RData")
 
 
 
-# sourcing in Zhen's merge_distCBD function
 
-res = merge_distCBD(train_append_weather, test_append_weather)
-
-train_merge_DistCBD = res$train_merge_DistCBD
-
-test_merge_DistCBD = res$test_merge_DistCBD
+# function to fix up train_complete and test_complete
 
 
 
-# merge in Jimmy's feature engineering
+rename_reorder <- function(train_complete_pre, test_complete_pre) {
+  
+  names(train_complete_pre)[names(train_complete_pre) == "GeoID"] <- "GeoId"
+  names(test_complete_pre)[names(test_complete_pre) == "GeoID"] <- "GeoId"
+  
+  train_complete = select(train_complete_pre, RowId, GeoId, IntersectionCity, everything())
+  test_complete = select(test_complete_pre, RowId, GeoId, IntersectionCity, everything())
+  
+  return(list(train_complete = train_complete, test_complete = test_complete))
+  
+}
 
-## merge in turn angles 
-
-train_turn_angle = read.csv("feature_engineering/data_files/train_turn-angle.csv", header = FALSE, col.names = c("RowId", "TurnAngle"))
-test_turn_angle = read.csv("feature_engineering/data_files/test_turn-angle.csv", header = FALSE, col.names = c("RowId", "TurnAngle"))
-
-train_merge_TurnAngle = merge(train_merge_DistCBD, train_turn_angle, by = "RowId")
-test_merge_TurnAngle = merge(test_merge_DistCBD, test_turn_angle, by = "RowId")
 
 
 
-## merge in one-way indicator and city-IntersectionID 
+# constructing the plan
 
-train_id = read.csv("feature_engineering/data_files/train-IDs.csv", header = TRUE)
-test_id = read.csv("feature_engineering/data_files/test-IDs.csv", header = TRUE)
+complete_data_plan <- drake_plan(
+  
+  res = merge_distCBD(train_append_weather, test_append_weather),
+  
+  train_merge_DistCBD = res$train_merge_DistCBD,
+  
+  test_merge_DistCBD = res$test_merge_DistCBD,
+  
+  # merge in Jimmy's feature engineering
+  
+  # merge in turn angles 
+  
+  train_turn_angle = read.csv("feature_engineering/data_files/train_turn-angle.csv", header = FALSE, col.names = c("RowId", "TurnAngle")),
+  test_turn_angle = read.csv("feature_engineering/data_files/test_turn-angle.csv", header = FALSE, col.names = c("RowId", "TurnAngle")),
+  
+  train_merge_TurnAngle = merge(train_merge_DistCBD, train_turn_angle, by = "RowId"),
+  test_merge_TurnAngle = merge(test_merge_DistCBD, test_turn_angle, by = "RowId"),
+  
+  ## merge in one-way indicator and city-IntersectionID 
+  
+  train_id = read.csv("feature_engineering/data_files/train-IDs.csv", header = TRUE),
+  test_id = read.csv("feature_engineering/data_files/test-IDs.csv", header = TRUE),
+  
+  train_entry_oneway = read.csv("feature_engineering/data_files/train-entry-oneway.csv", header = TRUE),
+  test_entry_oneway = read.csv("feature_engineering/data_files/test-entry-oneway.csv", header = TRUE),
+  
+  train_exit_oneway = read.csv("feature_engineering/data_files/train-exit-oneway.csv", header = TRUE),
+  test_exit_oneway = read.csv("feature_engineering/data_files/test-exit-oneway.csv", header = TRUE),
+  
+  train_append = data.frame(train_id, OnewayEntry = train_entry_oneway$is_oneway_entry, 
+                            OnewayExit = train_exit_oneway$is_oneway_exit),
+  test_append = data.frame(test_id, OnewayEntry = test_entry_oneway$is_oneway_entry, 
+                           OnewayExit = test_exit_oneway$is_oneway_exit),
+  
+  train_complete_pre = merge(train_merge_TurnAngle, train_append, by = "RowId"),
+  test_complete_pre = merge(test_merge_TurnAngle, test_append, by = "RowId"),
+  
+  res2 = rename_reorder(train_complete_pre, test_complete_pre),
+  
+  train_complete = res2$train_complete,
+  test_complete = res2$test_complete
+  
+)
 
-train_entry_oneway = read.csv("feature_engineering/data_files/train-entry-oneway.csv", header = TRUE)
-test_entry_oneway = read.csv("feature_engineering/data_files/test-entry-oneway.csv", header = TRUE)
+config <- drake_config(complete_data_plan)
 
-train_exit_oneway = read.csv("feature_engineering/data_files/train-exit-oneway.csv", header = TRUE)
-test_exit_oneway = read.csv("feature_engineering/data_files/test-exit-oneway.csv", header = TRUE)
+vis_drake_graph(config)
 
-train_append = data.frame(train_id, OnewayEntry = train_entry_oneway$is_oneway_entry, 
-                          OnewayExit = train_exit_oneway$is_oneway_exit)
-test_append = data.frame(test_id, OnewayEntry = test_entry_oneway$is_oneway_entry, 
-                          OnewayExit = test_exit_oneway$is_oneway_exit)
 
-train_complete = merge(train_merge_TurnAngle, train_append, by = "RowId")
-test_complete = merge(test_merge_TurnAngle, test_append, by = "RowId")
 
-names(train_complete)[names(train_complete) == "GeoID"] <- "GeoId"
-names(test_complete)[names(test_complete) == "GeoID"] <- "GeoId"
+make(complete_data_plan)
 
-train_complete = select(train_complete, RowId, GeoId, IntersectionCity, everything())
-test_complete = select(test_complete, RowId, GeoId, IntersectionCity, everything())
+history <- drake_history(analyze = TRUE)
+
+cache <- drake_cache()
 
 
 
