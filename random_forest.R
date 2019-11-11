3220e741798a769a271514ce853b4809d3dbd190
AlvinSheng <as133@rice.edu>
2019-11-10 21:13:29 -0500

    Slight renaming

--- random_forest.R
+++ random_forest.R
@@ -1,5 +1,5 @@
 
-geotab_random_forest <- function(train_complete, test_complete) {
+geotab_random_forest <- function(train, test) {
 
 # taking out variables I don't want RandomForest to use
 
@@ -8,13 +8,13 @@ geotab_random_forest <- function(train_complete, test_complete) {
 # EntryStreetName and ExitStreetName and Path have too many categories
 # RowId and IntersectionId should not be there
 
-model_dat = select(train_complete, -ends_with("Raw"), -starts_with("TotalTime"), -starts_with("TimeFrom"),
+model_dat = select(train, -ends_with("Raw"), -starts_with("TotalTime"), -starts_with("TimeFrom"),
                    -starts_with("DistanceTo"), -ends_with("StreetName"), -Path, -RowId, -IntersectionId, -IntersectionCity)
 
-sub_idx = sample(1:dim(train_complete)[1], size = dim(train_complete)[1] / 100)
+sub_idx = sample(1:dim(train)[1], size = dim(train)[1] / 100)
 
 # I'm using TotalTimeStopped as the response variable for the Random Forest
-rf_total_time_50 = randomForest(train_complete$TotalTimeStopped_p50[sub_idx] ~ ., data = model_dat[sub_idx,], importance = TRUE)
+rf_total_time_50 = randomForest(train$TotalTimeStopped_p50[sub_idx] ~ ., data = model_dat[sub_idx,], importance = TRUE)
 
 imps = importance(rf_total_time_50)   
 
