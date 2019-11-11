bc4f177cf3dc9948aef02153af6702498851a538
AlvinSheng <as133@rice.edu>
2019-11-10 21:22:27 -0500

    If you need help installing drake

--- modeling.R
+++ modeling.R
@@ -1,13 +1,20 @@
 # load dependencies
 
-library(randomForest)
+# If it's your first time using drake, use below code
+# library(devtools)
+# install_github("ropensci/drake")
 library(drake)
 library(dplyr)
 library(Matrix)
+
+# modeling packages
 library(glmnet)
+library(randomForest)
 
 load("backup_data_files/train_complete.RData")
 load("backup_data_files/test_complete.RData")
+
+submission <- read.csv("bigquery-geotab-intersection-congestion-data/sample_submission.csv")
 
 source("random_forest.R")
 
