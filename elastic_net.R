94792985a43f2565f208bad6a7f4fad0370b4a81
AlvinSheng <as133@rice.edu>
2019-11-18 11:42:04 -0500

    Updating the train_set, test_set, and made train_en_select and test_en_select with elastic net chosen features

--- elastic_net.R
+++ elastic_net.R
@@ -11,18 +11,19 @@ geotab_elastic_net <- function(train, test, submission) {
                            "TotalTimeStopped_p80", "DistanceToFirstStop_p20", 
                            "DistanceToFirstStop_p50", "DistanceToFirstStop_p80")
   
-  # Elastic Net
-  
   indep_vars_train = select(train, -RowId, -IntersectionId, -starts_with("TotalTime"), -starts_with("TimeFrom"),
-                            -starts_with("DistanceTo"), -Path, -ends_with("Raw"))
-  vars_test = select(test, -RowId, -IntersectionId, -Path, -ends_with("Raw"))
+                            -starts_with("DistanceTo"), -Path, -ends_with("Raw"), -GeoId, -GeoIdTrunc, -IntersectionCity, 
+                            -EntryStreetName, -ExitStreetName)
+  vars_test = select(test, -RowId, -IntersectionId, -Path, -ends_with("Raw"), -GeoId, -GeoIdTrunc, -IntersectionCity, 
+                     -EntryStreetName, -ExitStreetName)
   
   # Preparing for glmnet
-  
   train_mat <- sparse.model.matrix(~ ., indep_vars_train)[,-1]
   test_mat <- sparse.model.matrix(~ ., vars_test)[,-1]
   
+  # making sure dummy variables align between train and test
   train_mat <- train_mat[,colnames(train_mat) %in% colnames(test_mat)]
+  test_mat <- test_mat[,colnames(test_mat) %in% colnames(train_mat)]
   
   # elastic net regularization
   
@@ -38,27 +39,45 @@ geotab_elastic_net <- function(train, test, submission) {
   en_res <- glmnet(train_mat, responses, alpha = .95, lambda = en_cv$lambda.1se, 
                    family = "mgaussian", standardize.response = FALSE)
   
-  # construct sparse train and test sparse matrices with features selected by regular elastic net
-  
-  selected <- coef(en_res)
-  
-  train_en_select <- list()
-  test_en_select <- list()
-  
-  for (i in 1:length(selected)) {
-    
-    sel <- as.numeric(selected[[i]])
-    
-    sel_idx <- which(sel[-1] != 0)
-    
-    train_en_select[[i]] <- train_mat[,sel_idx]
-    test_en_select[[i]] <- test_mat[,sel_idx]
-    
-  }
-  
-  return(list(train_mat = train_mat, test_mat = test_mat, en_cv = en_cv, en_res = en_res, 
-              train_en_select = train_en_select, test_en_select = test_en_select))  
+  return(list(train_mat = train_mat, test_mat = test_mat, en_cv = en_cv, en_res = en_res))  
   
   
 }
 
+
+
+# returns 6 train and 6 test sparse matrices (for the 6 responses) with elastic-net selected features
+
+en_feature_selection <- function(elastic_net_results) {
+  
+  en_res <- elastic_net_results$en_res
+  
+  train_mat <- elastic_net_results$train_mat
+  
+  test_mat <- elastic_net_results$test_mat
+  
+  
+  
+  
+  # construct sparse train and test sparse matrices with features selected by regular elastic net
+  
+  # as it turns out, the same predictors have been chosen for every response! However, different coefficient
+  # values were used, so still good idea to fit 6 separate models for every response
+  
+  selected <- coef(en_res)
+  
+  # arbitrarily choose selected predictors for 2nd response--doesn't matter, as same predictors chosen
+  # across the 6 responses
+  
+  sel <- as.numeric(selected[[2]])
+  
+  sel_idx <- which(sel[-1] != 0)
+  
+  train_en_select <- train_mat[,sel_idx]
+  test_en_select <- test_mat[,sel_idx]
+  
+  
+  
+  return(list(train_en_select = train_en_select, test_en_select = test_en_select))
+  
+}
--- modeling.R
+++ modeling.R
@@ -24,12 +24,13 @@ source("elastic_net.R")
 
 modeling_plan <- drake_plan(
   
-  lin_reg_results = geotab_elastic_net(train_complete, test_complete, submission),
+  lin_reg_results = geotab_linear_regression(train_complete, test_complete, submission),
   submission_linear_regression = lin_reg_results$submission_linear_regression,
   
   elastic_net_results = geotab_elastic_net(train_complete, test_complete, submission),
-  train_en_select = elastic_net_results$train_en_select,
-  test_en_select = elastic_net_results$test_en_select,
+  ft_select_results = en_feature_selection(elastic_net_results),
+  train_en_select = ft_select_results$train_en_select,
+  test_en_select = ft_select_results$test_en_select,
   
   rf_results = geotab_random_forest(train_complete, test_complete),
   imps = rf_results$imps
@@ -54,54 +55,25 @@ cache <- drake_cache()
 loadd(imps)
 save(imps, file = "imps.RData")
 
-loadd(submission_linear_regression)
-write.csv(submission, file = "submission_files/submission_linear_regression.csv", row.names = FALSE)
+# loadd(submission_linear_regression)
+# write.csv(submission, file = "submission_files/submission_linear_regression.csv", row.names = FALSE)
 
-loadd(train_en_select)
-save(train_en_select, file = "modeling_files/train_en_select.RData")
-
-loadd(test_en_select)
-save(test_en_select, file = "modeling_files/test_en_select.RData")
-
-
-
-
-# ad hoc code to circumvent Error: vector memory exhausted (limit reached?)
-
-loadd(lin_reg_results)
-
-vars_test = select(test_complete, -RowId, -IntersectionId, -Path, -ends_with("Raw"))
+# loadd(train_en_select)
+# save(train_en_select, file = "modeling_files/train_en_select.RData")
+# 
+# loadd(test_en_select)
+# save(test_en_select, file = "modeling_files/test_en_select.RData")
 
 
 
-en_res <- lin_reg_results$en_res
-
-selected <- coef(en_res)
-
-sel <- as.numeric(selected[[2]])
-
-sel_idx <- which(sel[-1] != 0)
 
 
-
-train_mat <- lin_reg_results$train_mat
-
-test_mat <- sparse.model.matrix(~ ., vars_test)[,-1]
-
-
-
-# whoops--I forgot to make sure train_mat and test_mat had the same columns. Oh well-- train_en_select should be good tho
-
-train_en_select_TotalTimeStopped_p50 <- train_mat[,sel_idx]
-test_en_select_TotalTimeStopped_p50 <- test_mat[,sel_idx]
-
-
-
-save(test_mat, file = "modeling_files/test_mat.RData")
-save(train_en_select_TotalTimeStopped_p50, file = "modeling_files/train_en_select_TotalTimeStopped_p50.RData")
-save(test_en_select_TotalTimeStopped_p50, file = "modeling_files/test_en_select_TotalTimeStopped_p50.RData")
+# helpful code to deal with sparse matrix
 
 # head(train_en_select_TotalTimeStopped_p50[,!grepl("GeoId|IntersectionCity|EntryStreetName|ExitStreetName", colnames(train_en_select_TotalTimeStopped_p50))])
 
+# head(train_en_select[[1]][,!grepl("GeoId|IntersectionCity|EntryStreetName|ExitStreetName", colnames(train_en_select[[1]]))])
+
+# lm(train_complete$TotalTimeStopped_p50 ~ ., data = data.frame(as.matrix(test_mat)))
 
 
